59abd2b05ea5ce732c9654fdaf1720c9c0a100b4
Marharyta23 <144152456+Marharyta23@users.noreply.github.com>
2024-03-30 12:41:58 +0200

    fin

--- src/components/PhotosGallery/PhotosGallery.jsx
+++ src/components/PhotosGallery/PhotosGallery.jsx
@@ -1,3 +1,16 @@
-export const PhotosGallery = () => {
-  return <h3>PhotosGallery</h3>;
+import { Grid, PhotosGalleryItem } from 'components';
+
+export const PhotosGallery = ({ photos }) => {
+  return (
+    <Grid>
+      {photos.map(({ id, avg_color, alt, src }) => (
+        <PhotosGalleryItem key={id} avg_color={avg_color} alt={alt} src={src} />
+      ))}
+    </Grid>
+  );
 };
+
+// - `id` - унікальний ідентифікатор
+// - `avg_color` - колір фотографії,
+// - `alt` - опис фото,
+// - `src` - об'єкт з розмірами картинок, нам цікавий розмір `large`
--- src/tabs/Photos.jsx
+++ src/tabs/Photos.jsx
@@ -1,10 +1,44 @@
-// import { getPhotos } from 'apiService/photos';
-import { Text } from 'components';
+import { getPhotos } from 'apiService/photos';
+import { Text, Form, PhotosGallery, Button } from 'components';
+import { useState, useEffect } from 'react';
 
 export const Photos = () => {
+  const [query, setQuery] = useState('');
+  const [page, setPage] = useState(1);
+  const [photos, setPhotos] = useState([]);
+
+  const onFormSubmit = query => {
+    setQuery(query);
+    setPhotos([]);
+    setPage(1);
+  };
+
+  useEffect(() => {
+    if (!query) return;
+
+    const photosFromApi = async () => {
+      try {
+        const { photos } = await getPhotos(query, page);
+        setPhotos(prevPhotos => [...prevPhotos, ...photos]);
+      } catch (error) {
+        console.log(error.message);
+      }
+    };
+    photosFromApi();
+  }, [page, query]);
+
+  const onLoadMore = () => {
+    setPage(page + 1);
+  };
   return (
     <>
-      <Text textAlign="center">Let`s begin search 🔎</Text>
+      <Form onSubmit={onFormSubmit} />
+      {photos.length ? (
+        <PhotosGallery photos={photos} />
+      ) : (
+        <Text textAlign="center">Let`s begin search 🔎</Text>
+      )}
+      {photos.length > 0 && <Button onClick={onLoadMore}>Load more...</Button>}
     </>
   );
 };
